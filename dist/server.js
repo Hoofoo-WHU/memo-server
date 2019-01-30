da8652df92bb143a3fe68c5fcec832637deffabc
Hoofoo <wangshuophone@icloud.com>
2019-01-30 23:03:09 +0800

    重置leancloud秘钥

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,9 @@
 var koa = require('koa');
 var AV = require('leanengine');
 AV.init({
-    appId: process.env.LEANCLOUD_APP_ID || 'vj4CvrCv9g4kIbsv8C5Jtc4x-gzGzoHsz',
-    appKey: process.env.LEANCLOUD_APP_KEY || 'H1DlD4IGxglb63xLRCH8k3vg',
-    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'Lggj01tqdD7tSpNgTnLqCCSC'
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
 });
 var app = new koa();
 app.use(AV.koa());
