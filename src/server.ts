0b9ed9358284dfccfe4f7f317697c24863819937
Hoofoo <wangshuophone@icloud.com>
2019-01-31 01:31:32 +0800

    配置typescript

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
-import { Context } from "koa"
-
-
-var koa = require('koa')
-var AV = require('leanengine')
+import * as koa from 'koa'
+import * as AV from 'leanengine'
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  appId: process.env.LEANCLOUD_APP_ID!,
+  appKey: process.env.LEANCLOUD_APP_KEY!,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY!
 })
-
 var app = new koa()
-app.use(AV.koa())
-app.use((ctx: Context) => {
-  ctx.body = 'Hello, Word!!!'
+app.use(AV.koa() as koa.Middleware)
+app.use((ctx: koa.Context) => {
+  ctx.body = 'Hello, Word!'
 })
 app.listen(process.env.LEANCLOUD_APP_PORT || 3000)
