238c6904ff3c7c10ebc8f8420799d4ce8e3ecbd3
Hoofoo <wangshuophone@icloud.com>
2019-02-01 02:58:31 +0800

    实现GitHub登录

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,31 @@
+import login from '@/routes/login'
+import memo from '@/routes/memo'
 import Koa = require('koa')
 import bodyParser = require('koa-bodyparser')
 import convert = require('koa-convert')
 import logger = require('koa-logger')
 import Router = require('koa-router')
+import session = require('koa-session')
 import AV = require('leanengine')
 
-import memo from '@/routes/memo'
-
 const app = new Koa()
+app.keys = ['memo']
 app.use(convert(AV.koa() as Koa.Middleware))
 app.use(logger())
 app.use(bodyParser())
+app.use(session({ maxAge: 'session' }, app))
+
 app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*')
-  ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTION')
-  ctx.set('Access-Control-Allow-Headers', ' Content-Type')
+  ctx.set('Access-Control-Allow-Origin', ctx.header.origin)
+  ctx.set('Access-Control-Allow-Methods', 'PATCH,OPTION,POST,GET,DELETE')
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type')
+  ctx.set('Access-Control-Allow-Credentials', 'true')
   await next()
 })
 
 const api = new Router({ prefix: '/api' })
 api.use(memo.routes())
-
+api.use(login.routes())
 app.use(api.routes())
 
 export default app
