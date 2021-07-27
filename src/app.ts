a423d965d23c4c4463bc2e52dff426441d952944
Wang Shuo <wangshuobj@fenbi.com>
2021-07-28 06:11:43 +0800

    cors error

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,12 @@ app.use(bodyParser())
 app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', ctx.header.origin as any)
   ctx.set('Access-Control-Allow-Methods', 'PATCH,OPTIONS,POST,GET,DELETE')
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type')
+  ctx.set('Access-Control-Allow-Headers', ['Content-Type', 'Authorization'])
   ctx.set('Access-Control-Allow-Credentials', 'true')
   await next()
 })
 
-app.use(jwt({ secret: process.env.LEANCLOUD_APP_KEY!}).unless({ path: [/\/api\/login/, /\/api\/memo/] }))
+app.use(jwt({ secret: process.env.LEANCLOUD_APP_KEY!}).unless({ path: [/\/api\/login/] }))
 
 const api = new Router({ prefix: '/api' })
 api.use(memo.routes())
