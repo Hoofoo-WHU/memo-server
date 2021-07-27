472ba1d088e65a877c40e8608108a432b8c8d643
Wang Shuo <wangshuobj@fenbi.com>
2021-07-27 23:16:53 +0800

    fix: undefined 直接返回403

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -7,6 +7,10 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 
 router.post('/github/:code', async (ctx) => {
   if (!ctx.session.isLogin) {
+    if (!ctx.params.code) {
+      ctx.status = 403
+      return
+    }
     const res = await Axios({
       data: {
         client_id: GITHUB_CLIENT_ID,
